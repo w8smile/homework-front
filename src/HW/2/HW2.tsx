eaf540aadeb055f5c6e382a99804c656885d4a09
Serg Nakovnick <w8smile@gmail.com>
2023-12-18 14:57:53 +0300

    dz 2

--- src/HW/2/HW2.tsx
+++ src/HW/2/HW2.tsx
@@ -47,7 +47,7 @@ export const HW2 = () => {
   let [currentUsers, setCurrentUsers] = useState<UsersObjectType>(users);
 
   const filterUsers = () => {
-    const filteredUsers = users.myFriends.filter(user => user.address.city === 'San Francisco');
+    const filteredUsers = users.myFriends.filter(user => user.address.city === 'Los Angeles');
     setCurrentUsers({myFriends: filteredUsers });
   };
 
