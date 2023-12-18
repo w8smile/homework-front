f29e5636d58332b2e4e96a3df3ea901ab04f720e
Serg Nakovnick <w8smile@gmail.com>
2023-12-18 14:34:39 +0300

    dz 2

--- src/HW/2/CurrentUser.tsx
+++ src/HW/2/CurrentUser.tsx
@@ -1,14 +1,14 @@
-import {AddressType, UserType} from './HW2';
+import { UserType } from './HW2';
 
-export type CurrentUserPropsType = {
-    user: UserType
+type CurrentUserPropsType = {
+    user: UserType // пропиши типизацию
 };
 
 export const CurrentUser = (props: CurrentUserPropsType) => {
-  return (
-    <li key={props.user.id} id={`hw02-user-${props.user.id}`}>
-      <strong>{props.user.name}</strong> (Age: {props.user.age})<strong> Address: </strong>
-      {props.user.address.street}, {props.user.address.city}
-    </li>
-  );
+    return (
+        <li key={props.user.id} id={`hw02-user-${props.user.id}`}>
+            <strong>{props.user.name}</strong> (Age: {props.user.age})<strong> Address: </strong>
+            {props.user.address.street}, {props.user.address.city}
+        </li>
+    );
 };
